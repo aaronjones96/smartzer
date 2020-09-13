28373ea0bd24d35f8160687bead9864b0171a243
DESKTOP-60L5UTN\Aaron <aaron.unilever@gmail.com>
2020-09-13 16:48:31 +0100

    video controls

--- cypress/integration/webplayer/video_controls.js
+++ cypress/integration/webplayer/video_controls.js
@@ -1,11 +1,16 @@
-Cypress.Commands.add('playVideo', { prevSubject: 'element' }, playVideo)
+
 describe('Check video controls function', () => {
+
     it('Open video player and click play button', () => {
         cy.visit('https://player.smartzer.com/v/5695094313189376')
         cy.get('#playPause').click()
     })
-    if (video.paused) {
-        cy.wait(100)
-        cy.get('div.playbarButtonWrapper.styles_buttonWrapperBaseStyling__3TyK_.styles_order1__3I4-m', options).click()
-    }
+
+    it('click play/pause button', () => {
+        cy.get('div.playbarButtonWrapper.styles_buttonWrapperBaseStyling__3TyK_.styles_order1__3I4-m > button').should('have.attr', 'aria-label').get('div.playbarButtonWrapper.styles_buttonWrapperBaseStyling__3TyK_.styles_order1__3I4-m > button').click()
+    })
+    it('Hover over the volume button and check slider becomes visible', () => {
+        cy.get('div.playbarButtonWrapper.styles_buttonWrapperBaseStyling__3TyK_.styles_order2__2cKPC > div > button').trigger('mouseover')
+        cy.get('.slider').should('be.visible')
+    })
 })
